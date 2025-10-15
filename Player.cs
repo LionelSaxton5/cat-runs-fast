9e02c6df30273acb9b52dbc7b930d9d2ab0ec08f
yun778 <1016611460@qq.com>
2025-10-15 21:22:51 +0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -3,13 +3,15 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export] public float speed = 150; //移动速度
-	[Export] public float JumpVelocity = -1000f; //初始跳跃速度
+    [Export] public float speed = 150; //移动速度
+    [Export] public float JumpVelocity = -1000f; //初始跳跃速度
     [Export] public float BasicGravity = 2000f; //基础重力
-	[Export] public float VariableGravity = 3000f; //可变重力
+    [Export] public float VariableGravity = 3000f; //可变重力
 
-	private AnimatedSprite2D person; //人物动画节点
+    //动画播放相关
+    private AnimatedSprite2D person; //人物动画节点
     private AnimatedSprite2D cat; //猫咪动画节点
+    private string Currentanimation = ""; //当前动画名称
 
     private bool isfacingright = true; //是否面向右侧
 
@@ -25,13 +27,15 @@ public partial class Player : CharacterBody2D
     private bool jumpKeyReleased = false; // 跳跃键是否已释放
 
     public override void _Ready()
-	{
+    {
         person = GetNode<AnimatedSprite2D>("Person");
         cat = GetNode<AnimatedSprite2D>("Cat");
+
+        cat.Visible = false; //初始不可见
     }
 
-	public override void _PhysicsProcess(double delta)
-	{
+    public override void _PhysicsProcess(double delta)
+    {
         float deltaF = (float)delta;
         var velocity = Velocity; //初始化移动向量
 
@@ -43,7 +47,90 @@ public partial class Player : CharacterBody2D
         Velocity = velocity;
 
         MoveAndSlide(); //内置方法移动
-    }	
+    }
+
+    private enum CharacterStatus //人物状态枚举
+    {
+        idle,
+        walk
+    }
+
+    private enum CatStatus //猫咪状态枚举
+    {
+        attack1, attack2, climb, death, eat, hurt, idle, jump, lick, run, runjump, scare, sleep, slide, waking, walk
+    }
+
+    private void StateSelection() //状态选择方法
+    {
+        CharacterStatus character = new CharacterStatus();
+        CatStatus catStatus = new CatStatus();
+        if (person.Visible)
+        {
+            switch (character)
+            {
+                case CharacterStatus.idle:
+                    break;
+                case CharacterStatus.walk:
+                    break;
+            }
+        }
+        if (cat.Visible)
+        {
+            switch (catStatus)
+            {
+                case CatStatus.idle: //待机
+                    break;
+                case CatStatus.walk: //走路
+                    break;
+                case CatStatus.jump: //跳跃
+                    break;
+                case CatStatus.run: //奔跑
+                    break;
+                case CatStatus.runjump: //助跑跳
+                    break;
+                case CatStatus.attack1: //攻击1
+                    break;
+                case CatStatus.attack2: //攻击2
+                    break;
+                case CatStatus.climb: //攀爬
+                    break;
+                case CatStatus.death: //死亡
+                    break;
+                case CatStatus.eat: //吃东西
+                    break;
+                case CatStatus.hurt: //受伤
+                    break;
+                case CatStatus.scare: //吓唬
+                    break;
+                case CatStatus.sleep: //睡觉
+                    break;
+                case CatStatus.slide: //滑行
+                    break;
+                case CatStatus.waking: //醒来
+                    break;
+                case CatStatus.lick: //舔
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void AnimationPlayback(string animation) //动画播放方法
+    {
+        if (Currentanimation == animation) //动画相同则返回
+            return;
+        Currentanimation = animation;
+
+        if (person.Visible)
+        {
+            person.Play(animation);
+        }
+        else if (cat.Visible)
+        {
+            cat.Play(animation);
+        }
+    }
 
     private void HorizontalMovement(ref Vector2 velocity, float delta) //水平移动方法
     {
